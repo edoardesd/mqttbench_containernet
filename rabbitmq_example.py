84794c53de8888ea62dbcb5274d0e4a55a58bf4e
edoardesd <edoardesd@gmail.com>
2020-01-09 17:56:39 +0100

    rabbitmq images NOT clusterized

---
+++ rabbitmq_example.py
@@ -0,0 +1,77 @@
+"""
+Example topology with two containers (d1, d2),
+two switches, and one controller:
+
+          - (c)-
+         |      |
+(d1) - (s1) - (s2) - (d2)
+"""
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import info, error, setLogLevel
+import docker 
+import shlex
+import subprocess
+
+SIM_NAME = "RABBITMQ"
+
+setLogLevel('info')
+image_name = "flipperthedog/rabbitmq:ping"
+entrypoint_sh = "docker-entrypoint.sh rabbitmq-server"
+cmd = "rabbitmq-server"
+net = Containernet(controller=Controller)
+
+info('*** Adding controller\n')
+net.addController('c0', port=6654)
+
+info('*** Adding docker containers using {} images\n'.format(image_name))
+
+#port bindings is swapped (host_machine:docker_container)
+d1 = net.addDocker(hostname="rabbit1", name='d1', ip='10.0.0.251', dimage=image_name, 
+		volumes=["/home/longo/mqttbench_containernet/.erlang.cookie:/var/lib/rabbitmq/.erlang.cookie"])
+		#environment={"EMQX_NAME": "docker1",
+		#	     "EMQX_NODE__DIST_LISTEN_MAX": 6379,
+		#	     "EMQX_LISTENER__TCP__EXTERNAL": 1883,
+		#	     "EMQX_CLUSTER__DISCOVERY": "static",
+		#	     "EMQX_CLUSTER__STATIC__SEEDS": "docker2@172.17.0.3"})
+d1.cmd('echo "172.17.0.3      d2" >> /etc/hosts')
+
+d2 = net.addDocker(hostname="rabbit2", name='d2', ip='10.0.0.252', dimage=image_name, 
+		volumes=["/home/longo/mqttbench_containernet/.erlang.cookie:/var/lib/rabbitmq/.erlang.cookie"])
+		#environment={"EMQX_NAME": "docker1",
+		#	     "EMQX_NODE__DIST_LISTEN_MAX": 6379,
+		#	     "EMQX_LISTENER__TCP__EXTERNAL": 1883,
+		#	     "EMQX_CLUSTER__DISCOVERY": "static",
+		#	     "EMQX_CLUSTER__STATIC__SEEDS": "docker2@172.17.0.3"})
+d2.cmd('echo "172.17.0.2      d1" >> /etc/hosts')
+
+info('*** Starting {}\n'.format(SIM_NAME))
+client = docker.from_env()
+
+#d1_entry_out = client.containers.get('mn.d1').exec_run(entrypoint_sh, stdout=False, stderr=False)
+#info('*** Running entrypoint for mn.d1, {}\n'.format(d1_entry_out))
+
+#d2_entry_out = client.containers.get('mn.d2').exec_run(entrypoint_sh, stdout=False, stderr=False)
+#info('*** Running entrypoint for mn.d2, {}\n'.format(d2_entry_out))
+
+info('*** Adding switches\n')
+s1 = net.addSwitch('s1')
+s2 = net.addSwitch('s2')
+
+info('*** Creating links\n')
+net.addLink(d1, s1)
+net.addLink(s1, s2, cls=TCLink, delay='50ms', bw=1)
+net.addLink(s2, d2)
+
+info('*** Starting network\n')
+net.start()
+
+info('*** Testing connectivity\n')
+net.ping([d1, d2])
+
+info('*** Running CLI\n')
+CLI(net)
+info('*** Stopping network')
+net.stop()
