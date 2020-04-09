885d65bf397f8ba87b290519d24a4ba883faffa5
edoardesd <edoardesd@gmail.com>
2020-04-09 10:57:35 +0200

    deleted old files

--- tools/host_script.sh
+++
@@ -1,7 +0,0 @@
-CONTAINER_NAME=$1
-IP_NET=$(ip addr show "$CONTAINER_NAME"-eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
-
-head -n -1 /etc/hosts > ~/temp.txt
-cat ~/temp.txt > /etc/hosts
-rm ~/temp.txt
-echo "$IP_NET     $CONTAINER_NAME" >> /etc/hosts
