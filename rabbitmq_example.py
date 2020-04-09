ed1138b69bd672cc5f524b0fa0808003ec7fcba7
edoardesd <edoardesd@gmail.com>
2020-04-09 10:26:39 +0200

    add start function for entrypoints

--- rabbitmq_example.py
+++ rabbitmq_example.py
@@ -14,16 +14,25 @@ from mininet.log import info, error, setLogLevel
 import docker
 import shlex
 import subprocess
+import time
 import os
 
 SIM_NAME = "RABBITMQ"
-
 PWD = os.getcwd()
-
 setLogLevel('info')
 image_name = "flipperthedog/rabbitmq:ping"
-ENTRYPOINT = "docker-entrypoint.sh rabbitmq-server"
-cmd = "rabbitmq-server"
+
+
+def check_cluster():
+    broker_out = ""
+    iteration = 1
+    while "rabbit@d2" not in broker_out:
+        broker_out = d1.cmd("rabbitmqctl cluster_status")
+        info('{}) Waiting cluster start...\n'.format(iteration))
+        iteration += 1
+
+
+
 net = Containernet(controller=Controller)
 
 info('*** Adding controller\n')
@@ -31,47 +40,58 @@ net.addController('c0', port=6654)
 
 info('*** Adding docker containers using {} images\n'.format(image_name))
 
+# HOSTS
 # port bindings is swapped (host_machine:docker_container)
 d1 = net.addDocker(hostname="rabbit1", name='d1', ip='10.0.0.251', dimage=image_name,
                    port_bindings={5672: 5672},
                    volumes=[PWD + "/rabbitmq1.conf:/etc/rabbitmq/rabbitmq.conf"],
                    environment={"RABBITMQ_ERLANG_COOKIE": "GPLDKBRJYMSKLTLZQDVG"})
 
-d1.cmd('echo "172.17.0.3      d2" >> /etc/hosts')
+d1.cmd('echo "10.0.0.252      d2" >> /etc/hosts')
 
 d2 = net.addDocker(hostname="rabbit2", name='d2', ip='10.0.0.252', dimage=image_name,
                    port_bindings={5672: 5673},
                    volumes=[PWD + "/rabbitmq2.conf:/etc/rabbitmq/rabbitmq.conf"],
                    environment={"RABBITMQ_ERLANG_COOKIE": "GPLDKBRJYMSKLTLZQDVG"})
 
-d2.cmd('echo "172.17.0.2      d1" >> /etc/hosts')
+d2.cmd('echo "10.0.0.251      d1" >> /etc/hosts')
 
 
 info('*** Adding switches\n')
+# SWITCHES
 s1 = net.addSwitch('s1')
 s2 = net.addSwitch('s2')
 
 info('*** Creating links\n')
+# LINKS
 net.addLink(d1, s1)
-net.addLink(s1, s2, cls=TCLink, delay='1ms', bw=1)
+net.addLink(s1, s2, cls=TCLink, delay='50ms', bw=1)
 net.addLink(s2, d2)
 
 info('*** Starting network\n')
 net.start()
-
-info('*** Starting {}\n'.format(SIM_NAME))
-client = docker.from_env()
-
-info('*** Starting the entrypoints\n')
-info(client.containers.get('mn.d1').exec_run(ENTRYPOINT, stdout=False, stderr=False), '\n')
-info(client.containers.get('mn.d2').exec_run(ENTRYPOINT, stdout=False, stderr=False), '\n')
+net.staticArp()
 
 info('*** Testing connectivity\n')
 net.ping([d1, d2])
+
+info('*** Waiting network start up...\n')
+time.sleep(5)
+
+info('*** Starting the entrypoints\n')
+# START CONTAINERS
+d1.start()
+d2.start()
+
+info('*** Waiting boot (10 secs)...\n')
+time.sleep(10)
+
+# Check correct cluster configuration
+check_cluster()
+# It starts after 10 cycles
+info('*** Cluster created\n')
 
 info('*** Running CLI\n')
 CLI(net)
 info('*** Stopping network')
 net.stop()
-
-# still need to add the user
