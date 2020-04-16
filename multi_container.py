ad6d0bebf1443c87173163ed3aee79c429dbcaec
edoardesd <edoardesd@gmail.com>
2020-04-16 08:54:20 +0200

    add vernemq

--- multi_container.py
+++ multi_container.py
@@ -7,10 +7,10 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import debug, info, error, setLogLevel
 import argparse
+from shutil import copyfile
 import itertools
 import os
 import time
-
 
 PWD = os.getcwd()
 VERSION = 0.2
@@ -18,102 +18,142 @@ TOTAL_BROKERS = 3
 DELAY = 10
 IP_ADDR = '10.0.0.'
 IMAGES = {
-    "EMQX": "flipperthedog/emqx-bash:latest"
+    "EMQX": "flipperthedog/emqx-ip:latest",
+    "VERNEMQ": "francigjeci/vernemq-debian:latest",
+    "RABBITMQ": "flipperthedog/rabbitmq:ping"
 }
+
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='MQTT cluster simulation')
     parser.add_argument('-n', '--broker-number', dest='bkr_num', default=TOTAL_BROKERS,
                         help='specify the size of the cluster')
-    parser.add_argument('-t', '--type', dest='cluster_type', default='emqx',
+    parser.add_argument('-t', '--type', dest='cluster_type', default='rabbitmq',
                         help='broker type (EMQX, RABBITMQ, VERNEMQ, HIVEMQ)')
     parser.add_argument('-d', '--delay', dest='link_delay', default=DELAY,
                         help='delay over a simple link')
     return parser.parse_args()
 
 
-def emqx(broker):
-    print("EMQX test")
+def start_emqx(cont_name, cont_address, bind_ip, master_node):
+    return net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
+                         ports=[1883], port_bindings={1883: bind_ip},
+                         dimage=IMAGES["EMQX"],
+                         environment={"EMQX_NAME": cont_name,
+                                      "EMQX_HOST": cont_address,
+                                      "EMQX_NODE__DIST_LISTEN_MAX": 6379,
+                                      "EMQX_LISTENER__TCP__EXTERNAL": 1883,
+                                      "EMQX_CLUSTER__DISCOVERY": "static",
+                                      "EMQX_CLUSTER__STATIC__SEEDS": master_node
+                                      })
+
+
+def start_rabbitmq(cont_name, cont_address, bind_ip, master_node):
+    if master_node == "{}@{}".format(cont_name, cont_address):
+        info('Creating new rabbitmq config file\n')
+        dest_file = "{}/confiles/rabbitmq_all.conf".format(PWD)
+        copyfile(PWD + "/confiles/rabbitmq.conf", dest_file)
+        with open(dest_file, "a") as f:
+            f.write("cluster_formation.classic_config.nodes.1 = rabbit@{}\n".format(master_node[:master_node.index("@")]))
+
+
+    d = net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
+                         dimage=IMAGES["RABBITMQ"],
+                         port_bindings={1883: bind_ip},
+                         volumes=[PWD + "/confiles/rabbitmq_all_{}.conf:/etc/rabbitmq/rabbitmq.conf".format(cont_address[-1])],
+                         environment={"RABBITMQ_ERLANG_COOKIE": "GPLDKBRJYMSKLTLZQDVG"})
+
+    d.cmd('echo "{}      {}" >> /etc/hosts'.format(master_node[master_node.index("@")+1:], master_node[:master_node.index("@")]))
+    d.cmd('echo "10.0.0.253      rabbitmq_3" >> /etc/hosts')
+    d.cmd('echo "10.0.0.254      rabbitmq_4" >> /etc/hosts')
+
+    return d
+
+
+def start_hivemq(broker):
+    info('HiveMQ test')
+
+
+def start_vernemq(cont_name, cont_address, bind_ip, master_node):
+    return net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
+                         ports=[1883], port_bindings={1883: bind_ip},
+                         dimage=IMAGES["VERNEMQ"],
+                         environment={
+                             "DOCKER_VERNEMQ_NODENAME": cont_address,
+                             "DOCKER_VERNEMQ_DISCOVERY_NODE": master_node[master_node.index("@") + 1:],
+                             "DOCKER_VERNEMQ_ACCEPT_EULA": "yes",
+                             "DOCKER_VERNEMQ_ALLOW_ANONYMOUS": "on"
+                         })
+
+
+def invalid(broker):
+    info('Invalid {}'.format(broker))
+
+
+def cluster_type(argument):
+    switcher = {
+        'EMQX': start_emqx,
+        'RABBITMQ': start_rabbitmq,
+        'VERNEMQ': start_vernemq,
+        'HIVEMQ': start_hivemq
+    }
+    func = switcher.get(argument, lambda: invalid())
+
     local_list = []
 
+    my_master = None
     for cnt in range(2, args.bkr_num + 2):
         container_name = "{}_{}".format(args.cluster_type, cnt)
-        local_address = IP_ADDR+str(250+cnt)
-        d = net.addDocker(name=container_name, ip= local_address,
-                          # ports=[1883], port_bindings={1883: 1883},
-                          dimage=IMAGES[broker],
-                          environment={"EMQX_NAME": container_name,
-                                        "EMQX_HOST": local_address,
-                                        "EMQX_NODE__DIST_LISTEN_MAX": 6379,
-                                        "EMQX_LISTENER__TCP__EXTERNAL": 1883,
-                                        "EMQX_CLUSTER__DISCOVERY": "static",
-                                        "EMQX_CLUSTER__STATIC__SEEDS": "docker2@10.0.0.252"
-                                       })
-        local_list.append(d)
+        local_address = IP_ADDR + str(250 + cnt)
+        bind_addr = 1880 + cnt
+        if my_master is None:
+            my_master = "{}@{}".format(container_name, local_address)
+
+        local_list.append(func(container_name, local_address, bind_addr, my_master))
 
     return local_list
 
 
-def rabbitmq(broker):
-    print("RabbitMQ test")
-
-
-def hivemq(broker):
-    print("HiveMQ test")
-
-
-def vernemq(broker):
-    print("VerneMQ test")
-
-
-def invalid(broker):
-    print("Invalid {}".format(broker))
-
-
-def cluster_type(argument):
-
-    switcher = {
-        'EMQX': emqx,
-        'RABBITMQ': rabbitmq,
-        'VERNEMQ': vernemq,
-        'HIVEMQ': hivemq
-    }
-    func = switcher.get(argument, lambda: invalid())
-    return func(argument)
-
-
 def main(_args):
-    print("*** Multi containernet generator v{} ***".format(VERSION))
-    print("args:")
-    print("\tCLUSTER TYPE: {}".format(args.cluster_type))
-    print("\tNUMBER OF BROKERS: {}".format(args.bkr_num))
-    print("\tDELAY: {}".format(args.link_delay))
-    print("\n")
+    info('\n*** Multi containernet generator v{} ***'.format(VERSION))
+    info('\nargs:')
+    info('\n\tCLUSTER TYPE: {}'.format(args.cluster_type))
+    info('\n\tNUMBER OF BROKERS: {}'.format(args.bkr_num))
+    info('\n\tDELAY: {}'.format(args.link_delay))
+    info('\n')
 
-    info('*** Adding controller')
+    info('\n*** Adding controller')
     net.addController('c0', port=6654)
     info('  DONE\n')
 
-    info('*** Adding docker containers, type: {}\n'.format(_args.cluster_type.upper()))
+    info('\n*** Adding docker containers, type: {}\n'.format(_args.cluster_type.upper()))
     container_list = cluster_type(_args.cluster_type.upper())
-    print(container_list)
 
-    info('*** Adding switch\n')
+    info('\n*** Adding switch\t')
     s1 = net.addSwitch('s1')
+    info(s1)
 
-    info('*** Adding container-switch links\n')
+    info('\n*** Adding container-switch links\n')
     for c in container_list:
-        print(net.addLink(c, s1, delay='1ms', bw=1))
+        info(net.addLink(c, s1, cls=TCLink, delay='{}ms'.format(DELAY / 2), bw=1))
 
-    info('*** Starting network\n')
+    info('\n*** Starting network\n')
     net.start()
     net.staticArp()
 
-    print("\n")
-    info('*** Testing connectivity\n')
-    for subset in itertools.combinations(container_list, 2):
-        print(subset[0], subset[1], "\n")
-        net.ping([subset[0], subset[1]])
+    info('\n*** Testing connectivity\n')
+    net.pingAll()
+
+    info('\n*** Waiting the network start up...\n')
+    time.sleep(DELAY / 2)
+
+    info('\n*** Starting the entrypoints\n')
+    # START CONTAINERS
+    for c in container_list:
+        c.start()
+
+    info('*** Waiting the boot (10 secs)...\n')
+    time.sleep(DELAY)
 
     info('*** Running CLI\n')
     CLI(net)
