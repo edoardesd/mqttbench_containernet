85a92a16c33615c811c53d16d7a44585cfa1b157
edoardesd <edoardesd@gmail.com>
2020-04-30 23:17:13 +0200

    add working containers

--- mesh_routers.py
+++ mesh_routers.py
@@ -18,7 +18,7 @@ PWD = os.getcwd()
 VERSION = 0.2
 TOTAL_BROKERS = 5
 DELAY = 10
-IP_ADDR = '10.0.0.'
+IP_ADDR = '10.0.'
 IMAGES = {
     "EMQX": "flipperthedog/emqx-ip:latest",
     "VERNEMQ": "francigjeci/vernemq-debian:latest",
@@ -40,19 +40,20 @@ class LinuxRouter(Node):
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='MQTT cluster simulation')
-    parser.add_argument('-t', '--type', dest='cluster_type', default='rabbitmq',
+    parser.add_argument('-t', '--type', dest='cluster_type', default='emqx',
                         help='broker type (EMQX, RABBITMQ, VERNEMQ, HIVEMQ)')
     parser.add_argument('-d', '--delay', dest='link_delay', default=DELAY,
                         help='delay over a simple link', type=int)
     return parser.parse_args()
 
 
-def start_emqx(cont_name, cont_address, bind_ip, master_node):
+def start_emqx(cont_name, cont_address, bind_ip, master_node, default_route):
     return net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
-                         ports=[1883], port_bindings={1883: bind_ip},
+                         defaultRoute='via {}'.format(default_route),
                          dimage=IMAGES["EMQX"],
+                         ports=[1883], port_bindings={1883: bind_ip},
                          environment={"EMQX_NAME": cont_name,
-                                      "EMQX_HOST": cont_address,
+                                      "EMQX_HOST": cont_address[:-3],
                                       "EMQX_NODE__DIST_LISTEN_MAX": 6379,
                                       "EMQX_LISTENER__TCP__EXTERNAL": 1883,
                                       "EMQX_CLUSTER__DISCOVERY": "static",
@@ -60,36 +61,38 @@ def start_emqx(cont_name, cont_address, bind_ip, master_node):
                                       })
 
 
-def start_rabbitmq(cont_name, cont_address, bind_ip, master_node):
-    dest_file = "{}/confiles/rabbitmq_{}.conf".format(PWD, cont_address[-3:])
+def start_rabbitmq(cont_name, cont_address, bind_ip, master_node, default_route):
+    dest_file = "{}/confiles/rabbitmq_{}.conf".format(PWD, cont_address[5:-7])
     copyfile(PWD + "/confiles/rabbitmq.conf", dest_file)
     with open(dest_file, "a") as f:
         c = 0
-        for i in range(2, TOTAL_BROKERS + 2):
-            if IP_ADDR + str(200 + i) != cont_address:
+        for i in range(TOTAL_BROKERS):
+            _ip = "{}{}.100/24".format(IP_ADDR, i)
+            if _ip != cont_address:
                 c += 1
                 f.write("cluster_formation.classic_config.nodes.{} = rabbit@rabbitmq_{}\n".format(c, i))
 
     d = net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
-                      dimage=IMAGES["RABBITMQ"], \
+                      defaultRoute='via {}'.format(default_route),
+                      dimage=IMAGES["RABBITMQ"],
                       ports=[5672, 1883],
                       port_bindings={5672: 5670 + (bind_ip - 1880),
                                      1883: bind_ip},
                       volumes=[
-                          PWD + "/confiles/rabbitmq_{}.conf:/etc/rabbitmq/rabbitmq.conf".format(cont_address[-3:]),
+                          "{}:/etc/rabbitmq/rabbitmq.conf".format(dest_file),
                           PWD + "/confiles/enabled_plugins:/etc/rabbitmq/enabled_plugins"],
                       environment={"RABBITMQ_ERLANG_COOKIE": "GPLDKBRJYMSKLTLZQDVG"})
 
-    for i in range(2, TOTAL_BROKERS + 2):
-        if IP_ADDR + str(200 + i) != cont_address:
-            d.cmd('echo "{}      {}" >> /etc/hosts'.format(IP_ADDR + str(200 + i), "rabbitmq_" + str(i)))
+    for i in range(TOTAL_BROKERS):
+        _ip = "{}{}.100".format(IP_ADDR, i)
+        d.cmd('echo "{}      {}" >> /etc/hosts'.format(_ip, "rabbitmq_" + str(i)))
 
     return d
 
 
-def start_hivemq(cont_name, cont_address, bind_ip, master_node):
+def start_hivemq(cont_name, cont_address, bind_ip, master_node, default_route):
     source_config_file_path = os.path.join(PWD, 'confiles/config-dns.xml')
-    dest_file = "{}/confiles/config-dns_{}.xml".format(PWD, cont_address[-3:])
+    dest_file = "{}/confiles/config-dns_{}.xml".format(PWD, cont_address[5:-7])
 
     __port = 8000
 
@@ -103,8 +106,8 @@ def start_hivemq(cont_name, cont_address, bind_ip, master_node):
             cluster_nodes.remove(node)
 
         # Add all cluster's nodes into config file
-        _brokers_index = range(2, TOTAL_BROKERS + 2)
-        _brokers_addr = [IP_ADDR + str(200 + _broker_index) for _broker_index in range(2, TOTAL_BROKERS + 2)]
+        _brokers_index = range(TOTAL_BROKERS)
+        _brokers_addr = ["{}{}.100".format(IP_ADDR, _broker_index) for _broker_index in range(TOTAL_BROKERS)]
         for broker_addr in _brokers_addr:
             _new_node = ET.Element('node')
             _host = ET.Element('host')
@@ -118,22 +121,25 @@ def start_hivemq(cont_name, cont_address, bind_ip, master_node):
         config_file.write(dest_file)
 
         return net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
-                         ports=[1883], port_bindings={1883: bind_ip},
-                         dimage=IMAGES["HIVEMQ"],
-                         volumes=[
-                            PWD + "/confiles/config-dns_{}.xml:/opt/hivemq/conf/config.xml".format(cont_address[-3:])],
-                         environment={
-                             "HIVEMQ_BIND_ADDRESS": cont_address
-                         })
+                             defaultRoute='via {}'.format(default_route),
+                             ports=[1883], port_bindings={1883: bind_ip},
+                             dimage=IMAGES["HIVEMQ"],
+                             volumes=[
+                                 PWD + "/confiles/config-dns_{}.xml:/opt/hivemq/conf/config.xml".format(
+                                     cont_address[5:-7])],
+                             environment={
+                                 "HIVEMQ_BIND_ADDRESS": cont_address[:-3]
+                             })
 
 
-def start_vernemq(cont_name, cont_address, bind_ip, master_node):
+def start_vernemq(cont_name, cont_address, bind_ip, master_node, default_route):
     return net.addDocker(hostname=cont_name, name=cont_name, ip=cont_address,
-                         ports=[1883], port_bindings={1883: bind_ip},
+                         defaultRoute='via {}'.format(default_route),
                          dimage=IMAGES["VERNEMQ"],
+                         ports=[1883], port_bindings={1883: bind_ip},
                          environment={
-                             "DOCKER_VERNEMQ_NODENAME": cont_address,
-                             "DOCKER_VERNEMQ_DISCOVERY_NODE": master_node[master_node.index("@") + 1:],
+                             "DOCKER_VERNEMQ_NODENAME": cont_address[:-3],
+                             "DOCKER_VERNEMQ_DISCOVERY_NODE": master_node[master_node.index("@") + 1:-3],
                              "DOCKER_VERNEMQ_ACCEPT_EULA": "yes",
                              "DOCKER_VERNEMQ_ALLOW_ANONYMOUS": "on"
                          })
@@ -143,7 +149,7 @@ def invalid(broker):
     info('Invalid {}'.format(broker))
 
 
-def cluster_type(argument):
+def create_containers(argument, router_ips):
     switcher = {
         'EMQX': start_emqx,
         'RABBITMQ': start_rabbitmq,
@@ -155,14 +161,17 @@ def cluster_type(argument):
     local_list = []
 
     my_master = None
-    for cnt in range(2, TOTAL_BROKERS + 2):
-        container_name = "{}_{}".format(args.cluster_type, cnt)
-        local_address = IP_ADDR + str(200 + cnt)
-        bind_addr = 1880 + cnt
+
+    for count, ip in enumerate(router_ips):
+        default_route = ip[1].compressed
+        print(default_route)
+        container_name = "{}_{}".format(args.cluster_type, count)
+        local_address = "{}/24".format(ip[100].compressed)
+        bind_addr = 1880 + count
         if my_master is None:
             my_master = "{}@{}".format(container_name, local_address)
 
-        local_list.append(func(container_name, local_address, bind_addr, my_master))
+        local_list.append(func(container_name, local_address, bind_addr, my_master, default_route))
 
     return local_list
 
@@ -178,12 +187,11 @@ def run():
     info('*** Adding routers\n')
     router_networks = ['{}/24'.format(next(router.hosts())) for router in networks]
 
-    router_list = [net.addHost('r{}'.format(counter), cls=LinuxRouter, ip=router) for counter, router in enumerate(router_networks)]
-    print(router_list)
+    router_list = [net.addHost('r{}'.format(counter), cls=LinuxRouter, ip=router) for counter, router in
+                   enumerate(router_networks)]
 
     info('*** Adding switches\n')
     switch_list = [net.addSwitch('s{}'.format(s)) for s in range(TOTAL_BROKERS)]
-    print(switch_list)
 
     info('*** Adding host-switch links\n')
     for count, (s, r, n) in enumerate(zip(switch_list, router_list, router_networks)):
@@ -193,40 +201,54 @@ def run():
     # from 0 to all
     net.addLink(router_list[0], router_list[1], intfName1='r0-eth1', intfName2='r1-eth1',
                 params1={'ip': '10.10.0.1/24'},
-                params2={'ip': '10.10.0.2/24'})
+                params2={'ip': '10.10.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
     net.addLink(router_list[0], router_list[2], intfName1='r0-eth2', intfName2='r2-eth1',
                 params1={'ip': '10.11.0.1/24'},
-                params2={'ip': '10.11.0.2/24'})
+                params2={'ip': '10.11.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
     net.addLink(router_list[0], router_list[3], intfName1='r0-eth3', intfName2='r3-eth1',
                 params1={'ip': '10.12.0.1/24'},
-                params2={'ip': '10.12.0.2/24'})
+                params2={'ip': '10.12.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
+
     net.addLink(router_list[0], router_list[4], intfName1='r0-eth4', intfName2='r4-eth1',
                 params1={'ip': '10.13.0.1/24'},
-                params2={'ip': '10.13.0.2/24'})
+                params2={'ip': '10.13.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
 
     # from 1 to all
     net.addLink(router_list[1], router_list[2], intfName1='r1-eth2', intfName2='r2-eth2',
                 params1={'ip': '10.20.0.1/24'},
-                params2={'ip': '10.20.0.2/24'})
+                params2={'ip': '10.20.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
+
     net.addLink(router_list[1], router_list[3], intfName1='r1-eth3', intfName2='r3-eth2',
                 params1={'ip': '10.21.0.1/24'},
-                params2={'ip': '10.21.0.2/24'})
+                params2={'ip': '10.21.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
+
     net.addLink(router_list[1], router_list[4], intfName1='r1-eth4', intfName2='r4-eth2',
                 params1={'ip': '10.22.0.1/24'},
-                params2={'ip': '10.22.0.2/24'})
+                params2={'ip': '10.22.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
 
     # from 2 to all
     net.addLink(router_list[2], router_list[3], intfName1='r2-eth3', intfName2='r3-eth3',
                 params1={'ip': '10.30.0.1/24'},
-                params2={'ip': '10.30.0.2/24'})
+                params2={'ip': '10.30.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
+
     net.addLink(router_list[2], router_list[4], intfName1='r2-eth4', intfName2='r4-eth3',
                 params1={'ip': '10.31.0.1/24'},
-                params2={'ip': '10.31.0.2/24'})
+                params2={'ip': '10.31.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
 
     # from 3 to 4
     net.addLink(router_list[3], router_list[4], intfName1='r3-eth4', intfName2='r4-eth4',
                 params1={'ip': '10.40.0.1/24'},
-                params2={'ip': '10.40.0.2/24'})
+                params2={'ip': '10.40.0.2/24'},
+                cls=TCLink, delay='{}ms'.format(args.link_delay / 2), bw=1)
 
     info('*** Adding routing\n')
     # router 0
@@ -259,10 +281,10 @@ def run():
     router_list[4].cmd("ip route add 10.0.2.0/24 via 10.31.0.1 dev r4-eth3")
     router_list[4].cmd("ip route add 10.0.3.0/24 via 10.40.0.1 dev r4-eth4")
 
-    return switch_list
+    return switch_list, networks
 
 
-def main(_args):
+def main():
     info('\n*** Multi containernet generator v{} ***'.format(VERSION))
     info('\nargs:')
     info('\n\tCLUSTER TYPE: {}'.format(args.cluster_type))
@@ -270,15 +292,14 @@ def main(_args):
     info('\n\tDELAY: {}'.format(args.link_delay))
     info('\n')
 
-    s_list = run()
+    s_list, ip_routers = run()
 
-    info('\n*** Adding docker containers, type: {}\n'.format(_args.cluster_type.upper()))
-    container_list = cluster_type(_args.cluster_type.upper())
-
+    info('\n*** Adding docker containers, type: {}\n'.format(args.cluster_type.upper()))
+    container_list = create_containers(args.cluster_type.upper(), ip_routers)
 
     info('\n*** Adding container-switch links\n')
     for c, s in zip(container_list, s_list):
-        info(net.addLink(c, s, cls=TCLink, delay='{}ms'.format(_args.link_delay / 2), bw=1))
+        print(net.addLink(c, s))
 
     info('\n*** Starting network\n')
     net.start()
@@ -287,16 +308,16 @@ def main(_args):
     info('\n*** Testing connectivity\n')
     net.pingAll()
 
-    # info('\n*** Waiting the network start up ({} secs)...\n'.format(_args.link_delay / 2))
-    # time.sleep(_args.link_delay / 2)
-    #
-    # info('\n*** Starting the entrypoints\n')
-    # # START CONTAINERS
-    # for c in container_list:
-    #     c.start()
-    #
-    # info('*** Waiting the boot ({} secs)...\n'.format(_args.link_delay))
-    # time.sleep(_args.link_delay)
+    info('\n*** Waiting the network start up ({} secs)...\n'.format(args.link_delay / 2))
+    time.sleep(args.link_delay / 2)
+
+    info('\n*** Starting the entrypoints\n')
+    # START CONTAINERS
+    for c in container_list:
+        c.start()
+
+    info('*** Waiting the boot ({} secs)...\n'.format(args.link_delay))
+    time.sleep(args.link_delay)
 
     info('*** Running CLI\n')
     CLI(net)
@@ -311,4 +332,4 @@ if __name__ == "__main__":
 
     args = arg_parse()
 
-    main(args)
+    main()
