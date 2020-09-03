887b0d67eb814eb29b8736d16c58b53f55d0f389
edoardesd <edoardesd@gmail.com>
2020-09-03 11:46:53 +0200

    arg parser for simple pub

--- clients/simple_pub.py
+++ clients/simple_pub.py
@@ -1,18 +1,49 @@
-import paho.mqtt.client as mqtt
+import argparse
 import time
+import paho.mqtt.client as mqtt
 from datetime import datetime
 
 
-BROKER_ADDRESS = "172.17.0.4"
-TOPIC = "test"
-client = mqtt.Client("P1")
+def arg_parse():
+    parser = argparse.ArgumentParser(description='MQTT simple publisher', add_help=False)
+    parser.add_argument('-h', '--host', dest='host', default='10.0.1.100',
+                        help='broker host name (e.g. 10.0.0.100)')
+    parser.add_argument('-t', '--topic', dest='topic', default='test',
+                        help='mqtt topic')
+    parser.add_argument('-q', '--qos', dest='qos', default=2,
+                        help='mqtt quality of service', type=int)
+    parser.add_argument('-m', '--number-messages', dest='msg_num', default=20,
+                        help='numer of mqtt messages per client', type=int)
+    # parser.print_help()
 
-client.connect(BROKER_ADDRESS)
+    return parser.parse_args()
 
-for i in range(0, 20):
-    dateTimeObj = datetime.now()
-    now = timestampStr = dateTimeObj.strftime("%H:%M:%S.%f")[:-3]
-    print("published {}".format(now))
-    client.publish(TOPIC, str(now), qos=2)
 
-    time.sleep(1)
+def main():
+    client = mqtt.Client()
+
+    client.connect(args.host)
+    print("Connected to {}\n".format(args.host))
+    for i in range(0, args.msg_num):
+        now = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+        client.publish(args.topic, str(now), qos=args.qos)
+        print("{}) published {}".format(i, now))
+
+        time.sleep(1)
+
+    print("\nDONE!")
+
+
+if __name__ == "__main__":
+    args = arg_parse()
+    main()
+
+
+
+# complementary commands
+# ping: docker exec -it mn.emqx_0 ping 10.0.4.100 -c 20
+# sub: mosquitto_sub -h 172.17.0.2  -t test  -d | xargs -d$'\n' -L1 bash -c 'date "+%Y-%m-%d %T.%3N ---- $0"'  | tee test.txt
+
+
+# docker cp simple_pub.py mn.sub_2:/home/ubuntu/simple_pub.py
+# docker exec -it mn.sub_2 python simple_pub.py
