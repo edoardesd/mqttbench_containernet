7c16c83ee9ec3a182e4490e1022eaa0388405200
edoardesd <edoardesd@gmail.com>
2020-11-19 12:46:23 +0100

    reuse erlang cookie

--- mesh_routers.py
+++ mesh_routers.py
@@ -103,10 +103,10 @@ def start_rabbitmq(cont_name, cont_address, bind_ip, master_node, default_route,
                                      1883: bind_ip},
                       volumes=[
                           "{}:/etc/rabbitmq/rabbitmq.conf".format(dest_file),
-                          PWD + "/confiles/enabled_plugins:/etc/rabbitmq/enabled_plugins",
-                          PWD + "/confiles/erlang_cookie:/var/lib/rabbitmq/.erlang_cookie"],
+                          PWD + "/confiles/enabled_plugins:/etc/rabbitmq/enabled_plugins"],
                       mem_limit=args.ram_limit,
-                      cpuset_cpus=cpu)
+                      cpuset_cpus=cpu,
+                      environment={"RABBITMQ_ERLANG_COOKIE": "GPLDKBRJYMSKLTLZQDVG"})
 
     for i in range(TOTAL_BROKERS):
         _ip = "{}{}.100".format(IP_ADDR, i)
